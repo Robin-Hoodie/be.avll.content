611aeae783d9a376332c530e201ca8527b8578e0
Robin-Hoodie <robin@oreon.io>
2023-07-09 19:36:32 +0200

    add registration contests

---
+++ src/api/registration-contest/routes/registration-contest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * registration-contest router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::registration-contest.registration-contest');
