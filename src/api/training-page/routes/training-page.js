c61c13329531e99288936fb60e1ce2d6a124bce2
Robin-Hoodie <robin@oreon.io>
2023-08-11 15:11:40 +0200

    add training page

---
+++ src/api/training-page/routes/training-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * training-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::training-page.training-page');
---
+++ src/api/training-page/services/training-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * training-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::training-page.training-page');
