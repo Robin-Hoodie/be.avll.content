a679e4027c1a51cd1a1e89d007ccf99007010789
Robin-Hoodie <robin@oreon.io>
2023-08-26 08:12:29 +0200

    add links to blog articles

---
+++ src/api/blog-article-link/controllers/blog-article-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-article-link controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog-article-link.blog-article-link');
---
+++ src/api/blog-article-link/routes/blog-article-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-article-link router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blog-article-link.blog-article-link');
