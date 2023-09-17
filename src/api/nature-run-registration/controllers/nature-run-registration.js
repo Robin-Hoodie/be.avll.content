6a22612b0b1395ebaf51d5e1f07299ccad42d611
Robin-Hoodie <robin@oreon.io>
2023-09-17 19:50:49 +0200

    make t shirt optional in nature run

---
+++ src/api/nature-run-registration/controllers/nature-run-registration.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * nature-run-registration controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::nature-run-registration.nature-run-registration');
---
+++ src/api/nature-run-registration/routes/nature-run-registration.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * nature-run-registration router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::nature-run-registration.nature-run-registration');
